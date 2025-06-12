a20a7587a55a232a2a873ad6861ab6f496898cac
XianDe-Zeng <156569749+Zzeng0917@users.noreply.github.com>
2025-06-12 16:30:25 +0800

    Update gob.go

--- day1-codec/codec/gob.go
+++ day1-codec/codec/gob.go
@@ -37,19 +37,24 @@ func (c *GobCodec) ReadBody(body interface{}) error {
 func (c *GobCodec) Write(h *Header, body interface{}) (err error) {
 	defer func() {
 		_ = c.buf.Flush()
+		//将缓冲区的数据强制写入连接
 		if err != nil {
 			_ = c.Close()
 		}
-	}()
+		// 关闭连接
+	}() //无论程序以何种方式退出都执行
+
+	//序列化消息头为二进制数据，并写入缓冲区，若失败记录日志并返回错误
 	if err := c.enc.Encode(h); err != nil {
 		log.Println("rpc codec:gob error encoding header:", err)
 		return err
 	}
+	//类似地，序列化消息体
 	if err := c.enc.Encode(body); err != nil {
 		log.Println("rpc codec:gob error encoding body:", err)
 		return err
 	}
-	return nil
+	return nil //所有步骤成功，返回nil表示没有错误
 }
 
 func (c *GobCodec) Close() error {
